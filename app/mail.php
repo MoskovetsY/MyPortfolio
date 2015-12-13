93eef79b0d6167020a661aff1062793c76a4561a
Yuri Moskovets <moskovets.yuri@gmail.com>
2015-12-13 13:55:40 +0200

    Исходное положение

---
+++ app/mail.php
@@ -0,0 +1,12 @@
+<?php
+
+$recepient = "moskovets.yuri@gmail.com";
+$sitename = "Yuri Moskovets";
+
+$name = trim($_POST["name"]);
+$email = trim($_POST["email"]);
+$text = trim($_POST["text"]);
+$message = "Имя: $name \nEmail: $email \nТекст: $text";
+
+$pagetitle = "Новая заявка с сайта \"$sitename\"";
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
